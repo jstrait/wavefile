2ea4c26706de6d3abf78760740f7167c157d8d70
jstrait <joelstrait@hotmail.com>
2010-11-17 22:56:43 -0500

    Adding outline of new classes.

---
+++ wavefile_new.rb
@@ -0,0 +1,23 @@
+class WaveFileInfo
+  def initialize(file_name)
+  end
+end
+
+class WaveFileReader
+  def initialize(info)
+  end
+
+  def read(buffer_size)
+  end
+end
+
+class WaveFileWriter
+  def initialize(file_name)
+  end
+
+  def write(sample_data)
+  end
+
+  def close()
+  end
+end
