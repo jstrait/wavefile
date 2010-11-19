eec05b4f53dbd401fd3f06cf014739d31754b68f
jstrait <joelstrait@hotmail.com>
2010-11-18 20:03:15 -0500

    Adding WaveFileFormat class.

--- wavefile_new.rb
+++ wavefile_new.rb
@@ -3,8 +3,26 @@ class WaveFileInfo
   end
 end
 
+class WaveFileFormat
+  def initialize(channels, bits_per_sample, sample_rate)
+    @channels = channels
+    @bits_per_sample = @bits_per_sample
+    @sample_rate = sample_rate
+  end
+
+  def byte_rate()
+    return (@bits_per_sample / 8) * sample_rate
+  end
+
+  def block_align()
+    return (@bits_per_sample / 8) * @num_channels
+  end
+
+  attr_accessor :channels, :bits_per_sample, :sample_rate
+end
+
 class WaveFileReader
-  def initialize(info)
+  def initialize(format)
   end
 
   def read(buffer_size)
@@ -12,7 +30,7 @@ class WaveFileReader
 end
 
 class WaveFileWriter
-  def initialize(file_name)
+  def initialize(file_name, format)
   end
 
   def write(sample_data)
