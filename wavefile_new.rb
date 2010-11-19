e627c75fecc307f405a3f1b8bf4c6bc7227150f9
jstrait <joelstrait@hotmail.com>
2010-11-18 20:23:54 -0500

    Fleshing out WaveFileWriter.

--- wavefile_new.rb
+++ wavefile_new.rb
@@ -30,12 +30,54 @@ class WaveFileReader
 end
 
 class WaveFileWriter
+  CHUNK_ID = "RIFF"
+  FORMAT = "WAVE"
+  FORMAT_CHUNK_ID = "fmt "
+  SUB_CHUNK1_SIZE = 16
+  PCM = 1
+  DATA_CHUNK_ID = "data"
+  HEADER_SIZE = 36
+  PACK_CODES = {8 => "C*", 16 => "s*", 32 => "V*"}
+
   def initialize(file_name, format)
+    @file = File.open(file_name, "w")
+    @format = format
+
+    @pack_code = PACK_CODES[format.bits_per_sample]
+    write_header(sample_data_size)
   end
 
   def write(sample_data)
+    # TODO: Implement this.
+    #sample_data = convert(sample_data, format)
+
+    file.syswrite(sample_data.pack(@pack_code))
   end
 
   def close()
+    @file.sysseek(0)
+    write_header(sample_count)
+    
+    @file.close()
+  end
+
+private
+
+  def write_header(sample_data_size)
+    header = CHUNK_ID
+    header += [HEADER_SIZE + sample_data_size].pack("V")
+    header += FORMAT
+    header += FORMAT_CHUNK_ID
+    header += [SUB_CHUNK1_SIZE].pack("V")
+    header += [PCM].pack("v")
+    header += [@format.channels].pack("v")
+    header += [@format.sample_rate].pack("V")
+    header += [@format.byte_rate].pack("V")
+    header += [@format.block_align].pack("v")
+    header += [@format.bits_per_sample].pack("v")
+    header += DATA_CHUNK_ID
+    header += [sample_data_size].pack("V")
+
+    file.syswrite(header)
   end
 end
