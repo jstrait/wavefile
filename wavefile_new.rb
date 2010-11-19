01a343ab54b21fc8abbb3fa0f8744b3d01060fac
jstrait <joelstrait@hotmail.com>
2010-11-18 23:52:25 -0500

    Adding WaveFileBuffer.convert!() method, among other small changes.

--- wavefile_new.rb
+++ wavefile_new.rb
@@ -76,16 +76,27 @@ module WaveFile
   class WaveFileBuffer
     def initialize(samples, format)
       @samples = samples
-      @channels = format.channels
-      @bits_per_sample = format.bits_per_sample
-      @sample_rate = format.sample_rate
+      set_format(format)
     end
 
-    def convert(format)
+    def convert(new_format)
+      return WaveFileBuffer.new(@samples, format)
+    end
+
+    def convert!(new_format)
+      set_format(format)
       return self
     end
 
     attr_reader :samples, :channels, :bits_per_sample, :sample_rate
+
+  private
+
+    def set_format(format)
+      @channels = format.channels
+      @bits_per_sample = format.bits_per_sample
+      @sample_rate = format.sample_rate
+    end
   end
 
   class WaveFileReader
@@ -173,8 +184,7 @@ module WaveFile
     end
 
     def write(buffer)
-      converted_buffer = buffer.convert(@format)
-      samples = converted_buffer.samples
+      samples = buffer.convert(@format).samples
 
       @file.syswrite(samples.pack(@pack_code))
       @sample_count += samples.length
