b4746bf67faceee4b50530b7d66b10d94dddc6b1
jstrait <joelstrait@hotmail.com>
2011-04-10 18:06:19 -0400

    Adding ability to convert a buffer's number of channels.

--- lib/buffer.rb
+++ lib/buffer.rb
@@ -33,6 +33,33 @@ module WaveFile
   private
 
     def convert_buffer(samples, old_format, new_format)
+      new_samples = samples.dup
+      
+      new_samples = convert_buffer_channels(new_samples, old_format.channels, new_format.channels)
+
+      @format = new_format
+      
+      return new_samples
+    end
+
+    def convert_buffer_channels(samples, old_channels, new_channels)
+      # The cases of mono -> stereo and vice-versa are handled specially,
+      # because those conversion methods are faster than the general methods,
+      # and the large majority of wave files are expected to be either mono or stereo.
+      if old_channels == 1 && new_channels == 2
+        samples.map! {|sample| [sample, sample]}
+      elsif old_channels == 2 && new_channels == 1
+        samples.map! {|sample| (sample[0] + sample[1]) / 2}
+      elsif old_channels == 1 && new_channels >= 2
+        samples.map! {|sample| [].fill(sample, 0, new_channels)}
+      elsif old_channels >= 2 && new_channels == 1
+        samples.map! {|sample| sample.inject(0) {|sub_sample, sum| sum + sub_sample } / old_channels }
+      elsif old_channels > 2 && new_channels == 2
+        samples.map! {|sample| [sample[0], sample[1]]}
+      else
+        raise StandardError "Conversion of sample data from #{old_channels} channels to #{new_channels} channels is unsupported"
+      end
+    
       return samples
     end
   end
