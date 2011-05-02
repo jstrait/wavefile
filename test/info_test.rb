fe4965353a37a4fdbe1358817cb36b863b4e2aa3
jstrait <joelstrait@hotmail.com>
2011-05-02 14:56:17 -0400

    Adding tests for Info.calculate_duration()

--- test/info_test.rb
+++ test/info_test.rb
@@ -7,6 +7,8 @@ include WaveFile
 
 class InfoTest < Test::Unit::TestCase
   FILE_NAME = "foo.wav"
+  SECONDS_IN_MINUTE = 60
+  SECONDS_IN_HOUR = SECONDS_IN_MINUTE * 60
 
   def test_basic
     format = Format.new(2, 16, 44100)
@@ -20,6 +22,31 @@ class InfoTest < Test::Unit::TestCase
     assert_equal(4, info.block_align)
     assert_equal(44100, info.sample_count)
     assert_equal({:hours => 0, :minutes => 0, :seconds => 1, :milliseconds => 0}, info.duration)
+  end
+
+  def test_duration
+    # Test common sample rates (22050 and 44100), and some crazy arbitrary sample rate (12346)
+    [22050, 44100, 12346].each do |sample_rate|
+      format = Format.new(2, 16, sample_rate)
+
+      info = Info.new(FILE_NAME, format, 0)
+      assert_equal({:hours => 0, :minutes => 0, :seconds => 0, :milliseconds => 0}, info.duration)
+
+      info = Info.new(FILE_NAME, format, sample_rate / 2)
+      assert_equal({:hours => 0, :minutes => 0, :seconds => 0, :milliseconds => 500}, info.duration)
+
+      info = Info.new(FILE_NAME, format, sample_rate)
+      assert_equal({:hours => 0, :minutes => 0, :seconds => 1, :milliseconds => 0}, info.duration)
+
+      info = Info.new(FILE_NAME, format, sample_rate * SECONDS_IN_MINUTE)
+      assert_equal({:hours => 0, :minutes => 1, :seconds => 0, :milliseconds => 0}, info.duration)
+
+      info = Info.new(FILE_NAME, format, sample_rate * SECONDS_IN_HOUR)
+      assert_equal({:hours => 1, :minutes => 0, :seconds => 0, :milliseconds => 0}, info.duration)
+
+      info = Info.new(FILE_NAME, format, (sample_rate * SECONDS_IN_MINUTE) + sample_rate + (sample_rate / 2))
+      assert_equal({:hours => 0, :minutes => 1, :seconds => 1, :milliseconds => 500}, info.duration)
+    end
   end
 end
 
