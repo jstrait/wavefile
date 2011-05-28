e76213b0b14548853371f90249c1fbe297fec1ee
jstrait <joelstrait@hotmail.com>
2011-05-28 15:14:49 -0400

    Adding Reader test for opening non-existent file

--- test/reader_test.rb
+++ test/reader_test.rb
@@ -6,6 +6,10 @@ require 'wavefile.rb'
 include WaveFile
 
 class BufferTest < Test::Unit::TestCase
+  def test_nonexistent_file
+    assert_raise(Errno::ENOENT) { reader = Reader.new("test/fixtures/i_do_not_exist.wav") }
+  end
+
   def test_bad_riff_header
     assert_raise(UnsupportedFormatError) { reader = Reader.new("test/fixtures/bad_riff_header.wav") }
   end
