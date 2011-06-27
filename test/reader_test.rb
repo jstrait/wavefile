2193afa2835e3d38b25b17fbc94935050845a1f3
jstrait <joelstrait@hotmail.com>
2011-06-27 12:28:02 -0400

    Adding explanatory comments to Reader tests

--- test/reader_test.rb
+++ test/reader_test.rb
@@ -12,22 +12,27 @@ class ReaderTest < Test::Unit::TestCase
     assert_raise(Errno::ENOENT) { Reader.new(fixture("i_do_not_exist.wav")) }
   end
 
+  # File contains 0 bytes
   def test_empty_file
     assert_raise(UnsupportedFormatError) { Reader.new(fixture("empty.wav")) }
   end
 
+  # File consists of "RIFF" and nothing else
   def test_incomplete_riff_header
     assert_raise(UnsupportedFormatError) { Reader.new(fixture("incomplete_riff_header.wav")) }
   end
 
+  # First 4 bytes in the file are not "RIFF"
   def test_bad_riff_header
     assert_raise(UnsupportedFormatError) { Reader.new(fixture("bad_riff_header.wav")) }
   end
 
+  # The format code in the RIFF header is not "WAVE"
   def test_bad_wave_format
     assert_raise(UnsupportedFormatError) { Reader.new(fixture("bad_wavefile_format.wav")) }
   end
 
+  # The audio format in the format chunk is not 1 (i.e. PCM)
   def test_bad_audio_format
     assert_raise(UnsupportedFormatError) { Reader.new(fixture("bad_audio_format.wav")) }
   end
