3aec05e3f1adb519d35c062145dea3aa51c82cd6
jstrait <joelstrait@hotmail.com>
2011-06-27 12:25:20 -0400

    Removing duplicated fixture paths

--- test/reader_test.rb
+++ test/reader_test.rb
@@ -6,27 +6,35 @@ require 'wavefile.rb'
 include WaveFile
 
 class ReaderTest < Test::Unit::TestCase
+  FIXTURE_ROOT_PATH = "test/fixtures"
+
   def test_nonexistent_file
-    assert_raise(Errno::ENOENT) { Reader.new("test/fixtures/i_do_not_exist.wav") }
+    assert_raise(Errno::ENOENT) { Reader.new(fixture("i_do_not_exist.wav")) }
   end
 
   def test_empty_file
-    assert_raise(UnsupportedFormatError) { Reader.new("test/fixtures/empty.wav") }
+    assert_raise(UnsupportedFormatError) { Reader.new(fixture("empty.wav")) }
   end
 
   def test_incomplete_riff_header
-    assert_raise(UnsupportedFormatError) { Reader.new("test/fixtures/incomplete_riff_header.wav") }
+    assert_raise(UnsupportedFormatError) { Reader.new(fixture("incomplete_riff_header.wav")) }
   end
 
   def test_bad_riff_header
-    assert_raise(UnsupportedFormatError) { Reader.new("test/fixtures/bad_riff_header.wav") }
+    assert_raise(UnsupportedFormatError) { Reader.new(fixture("bad_riff_header.wav")) }
   end
 
   def test_bad_wave_format
-    assert_raise(UnsupportedFormatError) { Reader.new("test/fixtures/bad_wavefile_format.wav") }
+    assert_raise(UnsupportedFormatError) { Reader.new(fixture("bad_wavefile_format.wav")) }
   end
 
   def test_bad_audio_format
-    assert_raise(UnsupportedFormatError) { Reader.new("test/fixtures/bad_audio_format.wav") }
+    assert_raise(UnsupportedFormatError) { Reader.new(fixture("bad_audio_format.wav")) }
+  end
+
+private
+
+  def fixture(fixture_name)
+    return "#{FIXTURE_ROOT_PATH}/#{fixture_name}"
   end
 end
