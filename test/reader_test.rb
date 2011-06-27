a72764038bb34188789a8e7a7acbb58cd9671795
jstrait <joelstrait@hotmail.com>
2011-06-27 12:21:58 -0400

    Removing unnecessary assignments in tests

--- test/reader_test.rb
+++ test/reader_test.rb
@@ -7,26 +7,26 @@ include WaveFile
 
 class ReaderTest < Test::Unit::TestCase
   def test_nonexistent_file
-    assert_raise(Errno::ENOENT) { reader = Reader.new("test/fixtures/i_do_not_exist.wav") }
+    assert_raise(Errno::ENOENT) { Reader.new("test/fixtures/i_do_not_exist.wav") }
   end
 
   def test_empty_file
-    assert_raise(UnsupportedFormatError) { reader = Reader.new("test/fixtures/empty.wav") }
+    assert_raise(UnsupportedFormatError) { Reader.new("test/fixtures/empty.wav") }
   end
 
   def test_incomplete_riff_header
-    assert_raise(UnsupportedFormatError) { reader = Reader.new("test/fixtures/incomplete_riff_header.wav") }
+    assert_raise(UnsupportedFormatError) { Reader.new("test/fixtures/incomplete_riff_header.wav") }
   end
 
   def test_bad_riff_header
-    assert_raise(UnsupportedFormatError) { reader = Reader.new("test/fixtures/bad_riff_header.wav") }
+    assert_raise(UnsupportedFormatError) { Reader.new("test/fixtures/bad_riff_header.wav") }
   end
 
   def test_bad_wave_format
-    assert_raise(UnsupportedFormatError) { reader = Reader.new("test/fixtures/bad_wavefile_format.wav") }
+    assert_raise(UnsupportedFormatError) { Reader.new("test/fixtures/bad_wavefile_format.wav") }
   end
 
   def test_bad_audio_format
-    assert_raise(UnsupportedFormatError) { reader = Reader.new("test/fixtures/bad_audio_format.wav") }
+    assert_raise(UnsupportedFormatError) { Reader.new("test/fixtures/bad_audio_format.wav") }
   end
 end
