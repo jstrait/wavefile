029d854d4d0bbbb212b2be228aaffc772dcd4c8e
jstrait <joelstrait@hotmail.com>
2011-06-30 14:10:34 -0400

    Adding Writer tests for a basic file with some sample data, and for a file that requires a padding byte at the end.

--- test/writer_test.rb
+++ test/writer_test.rb
@@ -29,6 +29,34 @@ class WriterTest < Test::Unit::TestCase
     assert_equal(read_file(:expected, "no_samples.wav"), read_file(:actual, "no_samples.wav"))
   end
 
+  def test_basic
+    file_name = "valid_mono_16_44100.wav"
+    format = Format.new(1, 16, 44100)
+
+    writer = Writer.new("#{OUTPUT_FOLDER}/#{file_name}", format)
+    writer.write(Buffer.new([-10000, -10000, -10000, -10000, 10000, 10000, 10000, 10000] * 128, format))
+    writer.write(Buffer.new([-10000, -10000, -10000, -10000, 10000, 10000, 10000, 10000] * 128, format))
+    writer.write(Buffer.new([-10000, -10000, -10000, -10000, 10000, 10000, 10000, 10000] * 24, format))
+    writer.close()
+
+    assert_equal(read_file(:expected, file_name), read_file(:actual, file_name))
+  end
+
+  def test_with_padding_byte
+    file_name = "valid_mono_8_44100_with_padding_byte.wav"
+    format = Format.new(1, 8, 44100)
+
+    writer = Writer.new("#{OUTPUT_FOLDER}/#{file_name}", format)
+    writer.write(Buffer.new([88, 88, 88, 88, 167, 167, 167, 167] * 128, format))
+    writer.write(Buffer.new([88, 88, 88, 88, 167, 167, 167, 167] * 128, format))
+    writer.write(Buffer.new([88, 88, 88, 88, 167, 167, 167, 167] * 23 + [88, 88, 88, 88, 167, 167, 167], format))
+    writer.close()
+
+    assert_equal(read_file(:expected, file_name), read_file(:actual, file_name))
+  end
+
+private
+
   def read_file(type, file_name)
     # For Windows compatibility with binary files, File.read() is not directly used
     return File.open("test/fixtures/#{type}_output/#{file_name}", "rb") {|f| f.read() }
