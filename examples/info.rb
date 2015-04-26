d647ad5bd46d674223387a54af300f803b0f8620
jstrait <joel.strait@gmail.com>
2015-04-25 22:10:02 -0700

    Updating example for v0.7.0 API
    Reader.info() no longer exists as of v0.7.0

--- examples/info.rb
+++ examples/info.rb
@@ -1,18 +1,18 @@
 require 'wavefile'
 include WaveFile
 
-info = Reader.info(ARGV[0])
+reader = Reader.new(ARGV[0])
 
-puts "Metadata for #{info.file_name}:"
-puts "  Audio Format:        #{info.audio_format}"
-puts "  Channels:            #{info.channels}"
-puts "  Bits per sample:     #{info.bits_per_sample}"
-puts "  Samples per second:  #{info.sample_rate}"
-puts "  Bytes per second:    #{info.byte_rate}"
-puts "  Block align:         #{info.block_align}"
-puts "  Sample frame count:  #{info.sample_frame_count}"
+puts "Metadata for #{reader.file_name}:"
+puts "  Audio Format:        #{reader.native_format.audio_format}"
+puts "  Channels:            #{reader.native_format.channels}"
+puts "  Bits per sample:     #{reader.native_format.bits_per_sample}"
+puts "  Samples per second:  #{reader.native_format.sample_rate}"
+puts "  Bytes per second:    #{reader.native_format.byte_rate}"
+puts "  Block align:         #{reader.native_format.block_align}"
+puts "  Sample frame count:  #{reader.total_sample_frames}"
 
-duration = info.duration
+duration = reader.total_duration
 formatted_duration = duration.hours.to_s.rjust(2, "0") << ":" <<
                      duration.minutes.to_s.rjust(2, "0") << ":" <<
                      duration.seconds.to_s.rjust(2, "0") << ":" <<
