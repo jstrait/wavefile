7aca73454058d753d1513ad2800cddca110a6585
jstrait <joel.strait@gmail.com>
2016-03-05 18:39:07 -0800

    Enhancing metadata reading example to show if file is readable or not

--- examples/info.rb
+++ examples/info.rb
@@ -1,20 +1,30 @@
 require 'wavefile'
 include WaveFile
 
-reader = Reader.new(ARGV[0])
+file_name = ARGV[0]
+puts "Metadata for #{file_name}:"
 
-puts "Metadata for #{reader.file_name}:"
-puts "  Audio Format:        #{reader.native_format.audio_format}"
-puts "  Channels:            #{reader.native_format.channels}"
-puts "  Bits per sample:     #{reader.native_format.bits_per_sample}"
-puts "  Samples per second:  #{reader.native_format.sample_rate}"
-puts "  Bytes per second:    #{reader.native_format.byte_rate}"
-puts "  Block align:         #{reader.native_format.block_align}"
-puts "  Sample frame count:  #{reader.total_sample_frames}"
+begin
+  reader = Reader.new(file_name)
 
-duration = reader.total_duration
-formatted_duration = duration.hours.to_s.rjust(2, "0") << ":" <<
-                     duration.minutes.to_s.rjust(2, "0") << ":" <<
-                     duration.seconds.to_s.rjust(2, "0") << ":" <<
-                     duration.milliseconds.to_s.rjust(3, "0")
-puts "  Play time:           #{formatted_duration}"
+  puts "  Readable by this gem?  #{reader.readable_format? ? 'Yes' : 'No'}"
+  puts "  Audio Format:          #{reader.native_format.audio_format}"
+  puts "  Channels:              #{reader.native_format.channels}"
+  puts "  Bits per sample:       #{reader.native_format.bits_per_sample}"
+  puts "  Samples per second:    #{reader.native_format.sample_rate}"
+  puts "  Bytes per second:      #{reader.native_format.byte_rate}"
+  puts "  Block align:           #{reader.native_format.block_align}"
+  puts "  Sample frame count:    #{reader.total_sample_frames}"
+
+  duration = reader.total_duration
+  formatted_duration = duration.hours.to_s.rjust(2, "0") << ":" <<
+                       duration.minutes.to_s.rjust(2, "0") << ":" <<
+                       duration.seconds.to_s.rjust(2, "0") << ":" <<
+                       duration.milliseconds.to_s.rjust(3, "0")
+  puts "  Play time:             #{formatted_duration}"
+rescue InvalidFormatError
+  puts "  Not a valid Wave file!"
+end
+
+
+
