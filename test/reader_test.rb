9ef93281b043b5eb5499a011ec0a5891bf72cce1
Joel Strait <joel.strait@gmail.com>
2017-01-03 00:12:54 -0800

    Adding initial auto fixture for WAVEFORMATEXTENSIBLE format
    Should try to incorporate this test into the existing
    exhaustive tests, but for now just getting a test working
    so I can make sure fixture writing for WAVEFORMATEXTENSIBLE
    format is working correctly.

--- test/reader_test.rb
+++ test/reader_test.rb
@@ -182,6 +182,32 @@ class ReaderTest < Minitest::Test
     end
   end
 
+  def test_read_native_extensible_format
+    channels = :stereo
+    sample_format = :pcm_16
+
+    reader = Reader.new(fixture("valid/valid_extensible_stereo_pcm_16_44100.wav"))
+    assert_equal(2, reader.native_format.channels)
+    assert_equal(16, reader.native_format.bits_per_sample)
+    assert_equal(44100, reader.native_format.sample_rate)
+    assert_equal(2, reader.format.channels)
+    assert_equal(16, reader.format.bits_per_sample)
+    assert_equal(44100, reader.format.sample_rate)
+    assert_equal(false, reader.closed?)
+    assert_equal(0, reader.current_sample_frame)
+    assert_equal(2240, reader.total_sample_frames)
+    assert_equal(true, reader.readable_format?)
+    reader.close
+
+    buffers = read_file("valid/valid_extensible_stereo_pcm_16_44100.wav", 1024)
+
+    assert_equal(3, buffers.length)
+    assert_equal([1024, 1024, 192], buffers.map {|buffer| buffer.samples.length })
+    assert_equal(SQUARE_WAVE_CYCLE[channels][sample_format] * 128, buffers[0].samples)
+    assert_equal(SQUARE_WAVE_CYCLE[channels][sample_format] * 128, buffers[1].samples)
+    assert_equal(SQUARE_WAVE_CYCLE[channels][sample_format] * 24,  buffers[2].samples)
+  end
+
   def test_read_with_format_conversion
     buffers = read_file("valid/valid_mono_pcm_16_44100.wav", 1024, Format.new(:stereo, :pcm_8, 22100))
 
