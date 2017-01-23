b6f5ca9c06fe9fdb9ef29cd56dfba70273614ad3
Joel Strait <joel.strait@gmail.com>
2017-01-22 17:00:45 -0800

    Updating example to be simpler
    The buffer size is now optional, so it doesn't need to be
    manually specified.

--- examples/append.rb
+++ examples/append.rb
@@ -1,11 +1,9 @@
 require 'wavefile'
 include WaveFile
 
-BUFFER_SIZE = 4096
-
 Writer.new("append.wav", Format.new(:mono, :pcm_16, 44100)) do |writer|
   ARGV.each do |file_name|
-    Reader.new(file_name).each_buffer(BUFFER_SIZE) do |buffer|
+    Reader.new(file_name).each_buffer do |buffer|
       writer.write(buffer)
     end
   end
