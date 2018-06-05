188a75d8d412bfc32fce09fe7d7bb6170be608fe
Joel Strait <joel.strait@gmail.com>
2018-06-05 13:12:45 -0700

    Using write, not syswrite
    In practice I don't think this matters for this test, but trying
    to be consistent.

--- test/chunk_readers/format_chunk_reader_test.rb
+++ test/chunk_readers/format_chunk_reader_test.rb
@@ -7,13 +7,13 @@ include WaveFile::ChunkReaders
 class FormatChunkReaderTest < Minitest::Test
   def test_basic_pcm_no_extension
     io = StringIO.new
-    io.syswrite([1].pack(UNSIGNED_INT_16))   # Audio format
-    io.syswrite([2].pack(UNSIGNED_INT_16))   # Channels
-    io.syswrite([44100].pack(UNSIGNED_INT_32))   # Sample rate
-    io.syswrite([176400].pack(UNSIGNED_INT_32))   # Byte rate
-    io.syswrite([4].pack(UNSIGNED_INT_16))   # Block align
-    io.syswrite([16].pack(UNSIGNED_INT_16))   # Bits per sample
-    io.syswrite("data")   # Start of the next chunk
+    io.write([1].pack(UNSIGNED_INT_16))   # Audio format
+    io.write([2].pack(UNSIGNED_INT_16))   # Channels
+    io.write([44100].pack(UNSIGNED_INT_32))   # Sample rate
+    io.write([176400].pack(UNSIGNED_INT_32))   # Byte rate
+    io.write([4].pack(UNSIGNED_INT_16))   # Block align
+    io.write([16].pack(UNSIGNED_INT_16))   # Bits per sample
+    io.write("data")   # Start of the next chunk
     io.rewind
 
     format_chunk_reader = FormatChunkReader.new(io, 16)
@@ -33,13 +33,13 @@ class FormatChunkReaderTest < Minitest::Test
   # is still read properly.
   def test_gibberish_no_extension
     io = StringIO.new
-    io.syswrite([555].pack(UNSIGNED_INT_16))   # Audio format
-    io.syswrite([111].pack(UNSIGNED_INT_16))   # Channels
-    io.syswrite([12345].pack(UNSIGNED_INT_32))   # Sample rate
-    io.syswrite([9].pack(UNSIGNED_INT_32))   # Byte rate
-    io.syswrite([8000].pack(UNSIGNED_INT_16))   # Block align
-    io.syswrite([23433].pack(UNSIGNED_INT_16))   # Bits per sample
-    io.syswrite("data")   # Start of the next chunk
+    io.write([555].pack(UNSIGNED_INT_16))   # Audio format
+    io.write([111].pack(UNSIGNED_INT_16))   # Channels
+    io.write([12345].pack(UNSIGNED_INT_32))   # Sample rate
+    io.write([9].pack(UNSIGNED_INT_32))   # Byte rate
+    io.write([8000].pack(UNSIGNED_INT_16))   # Block align
+    io.write([23433].pack(UNSIGNED_INT_16))   # Bits per sample
+    io.write("data")   # Start of the next chunk
     io.rewind
 
     format_chunk_reader = FormatChunkReader.new(io, 16)
@@ -57,14 +57,14 @@ class FormatChunkReaderTest < Minitest::Test
 
   def test_basic_float_with_empty_extension
     io = StringIO.new
-    io.syswrite([3].pack(UNSIGNED_INT_16))   # Audio format
-    io.syswrite([2].pack(UNSIGNED_INT_16))   # Channels
-    io.syswrite([44100].pack(UNSIGNED_INT_32))   # Sample rate
-    io.syswrite([352800].pack(UNSIGNED_INT_32))   # Byte rate
-    io.syswrite([8].pack(UNSIGNED_INT_16))   # Block align
-    io.syswrite([32].pack(UNSIGNED_INT_16))   # Bits per sample
-    io.syswrite([0].pack(UNSIGNED_INT_16))   # Extension size
-    io.syswrite("data")   # Start of the next chunk
+    io.write([3].pack(UNSIGNED_INT_16))   # Audio format
+    io.write([2].pack(UNSIGNED_INT_16))   # Channels
+    io.write([44100].pack(UNSIGNED_INT_32))   # Sample rate
+    io.write([352800].pack(UNSIGNED_INT_32))   # Byte rate
+    io.write([8].pack(UNSIGNED_INT_16))   # Block align
+    io.write([32].pack(UNSIGNED_INT_16))   # Bits per sample
+    io.write([0].pack(UNSIGNED_INT_16))   # Extension size
+    io.write("data")   # Start of the next chunk
     io.rewind
 
     format_chunk_reader = FormatChunkReader.new(io, 18)
@@ -82,17 +82,17 @@ class FormatChunkReaderTest < Minitest::Test
 
   def test_wave_format_extensible
     io = StringIO.new
-    io.syswrite([65534].pack(UNSIGNED_INT_16))   # Audio format
-    io.syswrite([2].pack(UNSIGNED_INT_16))   # Channels
-    io.syswrite([44100].pack(UNSIGNED_INT_32))   # Sample rate
-    io.syswrite([264600].pack(UNSIGNED_INT_32))   # Byte rate
-    io.syswrite([6].pack(UNSIGNED_INT_16))   # Block align
-    io.syswrite([24].pack(UNSIGNED_INT_16))   # Bits per sample
-    io.syswrite([22].pack(UNSIGNED_INT_16))   # Extension size
-    io.syswrite([20].pack(UNSIGNED_INT_16))   # Valid bits per sample
-    io.syswrite([0].pack(UNSIGNED_INT_32))   # Channel mask
-    io.syswrite(SUB_FORMAT_GUID_PCM)
-    io.syswrite("data")   # Start of the next chunk
+    io.write([65534].pack(UNSIGNED_INT_16))   # Audio format
+    io.write([2].pack(UNSIGNED_INT_16))   # Channels
+    io.write([44100].pack(UNSIGNED_INT_32))   # Sample rate
+    io.write([264600].pack(UNSIGNED_INT_32))   # Byte rate
+    io.write([6].pack(UNSIGNED_INT_16))   # Block align
+    io.write([24].pack(UNSIGNED_INT_16))   # Bits per sample
+    io.write([22].pack(UNSIGNED_INT_16))   # Extension size
+    io.write([20].pack(UNSIGNED_INT_16))   # Valid bits per sample
+    io.write([0].pack(UNSIGNED_INT_32))   # Channel mask
+    io.write(SUB_FORMAT_GUID_PCM)
+    io.write("data")   # Start of the next chunk
     io.rewind
 
     format_chunk_reader = FormatChunkReader.new(io, 40)
@@ -113,13 +113,13 @@ class FormatChunkReaderTest < Minitest::Test
 
   def test_chunk_size_too_small
     io = StringIO.new
-    io.syswrite([1].pack(UNSIGNED_INT_16))   # Audio format
-    io.syswrite([2].pack(UNSIGNED_INT_16))   # Channels
-    io.syswrite([44100].pack(UNSIGNED_INT_32))   # Sample rate
-    io.syswrite([176400].pack(UNSIGNED_INT_32))   # Byte rate
-    io.syswrite([4].pack(UNSIGNED_INT_16))   # Block align
-    io.syswrite([16].pack(UNSIGNED_INT_16))   # Bits per sample
-    io.syswrite("data")   # Start of the next chunk
+    io.write([1].pack(UNSIGNED_INT_16))   # Audio format
+    io.write([2].pack(UNSIGNED_INT_16))   # Channels
+    io.write([44100].pack(UNSIGNED_INT_32))   # Sample rate
+    io.write([176400].pack(UNSIGNED_INT_32))   # Byte rate
+    io.write([4].pack(UNSIGNED_INT_16))   # Block align
+    io.write([16].pack(UNSIGNED_INT_16))   # Bits per sample
+    io.write("data")   # Start of the next chunk
     io.rewind
 
     format_chunk_reader = FormatChunkReader.new(io, 15)
