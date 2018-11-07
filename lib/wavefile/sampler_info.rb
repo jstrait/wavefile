cc925c0cb1deacff4cec2d4de89b62e954d49b79
Joel Strait <joel.strait@gmail.com>
2018-11-07 11:24:38 -0800

    Various documentation improvements for `SamplerInfo` class

--- lib/wavefile/sampler_info.rb
+++ lib/wavefile/sampler_info.rb
@@ -33,7 +33,7 @@ module WaveFile
     #         If not intended for a particular product, this should be 0.
     attr_reader :product_id
 
-    # Public: Returns the length of each sample in nanoseconds, which is typically `1 / sample rate`.
+    # Public: Returns the length of each sample in nanoseconds, which is typically <code>1 / sample rate</code>.
     #         For example, with a sample rate of 44100 this would be 22675 nanoseconds. However, this
     #         can be set to an arbitrary value to allow for fine tuning.
     attr_reader :sample_duration
@@ -42,7 +42,7 @@ module WaveFile
     attr_reader :midi_note
 
     # Public: Returns the number of cents up from the specified MIDI unity note field. 100 cents is equal to
-    #         one semitone. For example, if this value is 50, and `midi_note` is 60, then the sample is tuned
+    #         one semitone. For example, if this value is 50, and #midi_note is 60, then the sample is tuned
     #         half-way between MIDI note 60 and 61. If the value is 0, then the sample has no fine tuning.
     attr_reader :fine_tuning_cents
 
@@ -52,15 +52,14 @@ module WaveFile
     # Public: Returns a Hash representing the SMPTE time offset.
     attr_reader :smpte_offset
 
-    # Public: Returns an Array of 0 or more SampleLoop objects containing loop point info. Loop point info
+    # Public: Returns an Array of 0 or more SamplerLoop objects containing loop point info. Loop point info
     #         can indicate that (for example) the sampler should loop between a given sample range as long
     #         as the sample is played.
     attr_reader :loops
 
-    # Public: Returns a String of data specific to the intended target sampler. This is returned as a raw
-    #         sequencer of bytes, because the structure of this data depends on the specific sampler. If
-    #         you want to use it, you'll have to parse it yourself. If there is no sampler specific data,
-    #         this will be an empty string.
+    # Public: Returns a String of data specific to the intended target sampler, or nil if there is no sampler
+    #         specific data. This is returned as a raw String because the structure of this data depends on
+    #         the specific sampler. If you want to use it, you'll need to unpack the string yourself.
     attr_reader :sampler_specific_data
   end
 end
