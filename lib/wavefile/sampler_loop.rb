d230d744097168038c95b523729a4ff3ebbaaed7
Joel Strait <joel.strait@gmail.com>
2018-11-14 14:52:37 -0800

    Expanding `SamplerLoop` documentation

--- lib/wavefile/sampler_loop.rb
+++ lib/wavefile/sampler_loop.rb
@@ -1,12 +1,24 @@
 module WaveFile
   # Public: Provides a way to indicate the data about sampler loop points
   #         in a file's "smpl" chunk. That is, information about how a sampler
-  #         could loop between a sample  range while playing this *.wav as a note.
+  #         could loop between a sample range while playing this *.wav as a note.
   #         If a *.wav file contains a "smpl" chunk, then Reader.sample_info.loops
   #         will return an array of SamplerLoop objects with the relevant info.
-  #
-  # Returns a SamplerLoop containing the info in a file's "smpl" chunk.
   class SamplerLoop
+    # Public: Constructs a new SamplerLoop instance.
+    #
+    # id - A numeric ID which identifies the specific loop.
+    # type - Indicates which direction the loop should run. Should either be one of the symbols
+    #        +:forward+, +:alternating+, +:backward+, or a positive Integer. If an Integer, then 0 will
+    #        be normalized to +:forward+, 1 to +:alternating+, 2 to +:backward+, and 3 or greater will
+    #        be normalized to +:unknown+.
+    # start_sample_frame - The first sample frame in the loop.
+    # end_sample_frame - The last sample frame in the loop.
+    # fraction - A Float between 0.0 and 1.0 which specifies a fraction of a sample at which to loop.
+    #            This allows a loop to be fine tuned at a resolution finer than one sample.
+    # play_count - The number of times to loop. 0 means infinitely.
+    #
+    # Raises InvalidFormatError if the given arguments are invalid.
     def initialize(id:, type:, start_sample_frame:, end_sample_frame:, fraction:, play_count:)
       type = normalize_type(type)
 
@@ -32,11 +44,10 @@ module WaveFile
     #         are :forward, :alternating, :backward, or :unknown.
     attr_reader :type
 
-    # Public: Returns the start-position (in sample frames) of the loop
+    # Public: Returns the first sample frame of the loop.
     attr_reader :start_sample_frame
 
-    # Public: Returns the end-position (in sample frames) of the loop. The ending sample frame
-    #         should be included in the loop.
+    # Public: Returns the last sample frame of the loop.
     attr_reader :end_sample_frame
 
     # Public: A value between 0.0 and 1.0 which specifies a fraction of a sample at which to loop.
