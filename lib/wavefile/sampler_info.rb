7aa5c8c58269271ab303453849bef39e39a503a2
Joel Strait <joel.strait@gmail.com>
2018-11-14 15:17:09 -0800

    Additional documentation for `SamplerInfo`

--- lib/wavefile/sampler_info.rb
+++ lib/wavefile/sampler_info.rb
@@ -4,9 +4,31 @@ module WaveFile
   #         sampler, such as the file's MIDI note or loop points. If a *.wav
   #         file contains a "smpl" chunk, then Reader.sample_info will
   #         return an instance of this object with the relevant info.
-  #
-  # Returns a SamplerInfo containing the info in a file's "smpl" chunk.
   class SamplerInfo
+    # Public: Constructs a new SamplerInfo instance.
+    #
+    # manufacturer_id - the ID of the manufacturer that this sample is intended for. If it's not
+    #                   intended for a sampler from a particular manufacturer, this should be 0.
+    #                   See the list at https://www.midi.org/specifications-old/item/manufacturer-id-numbers
+    # product_id - the ID of the product made by the manufacturer this sample is intended for.
+    #              If not intended for a particular product, this should be 0.
+    # sample_duration - the length of each sample in nanoseconds, which is typically <code>1 / sample rate</code>.
+    #                   For example, with a sample rate of 44100 this would be 22675 nanoseconds. However, this
+    #                   can be set to an arbitrary value to allow for fine tuning.
+    # midi_note - the MIDI note number of the sample. Should be between 0 and 127.
+    # fine_tuning_cents - the number of cents up from the specified MIDI unity note field. 100 cents is equal to
+    #                     one semitone. For example, if this value is 50, and #midi_note is 60, then the sample is
+    #                     tuned half-way between MIDI note 60 and 61. If the value is 0, then the sample has no
+    #                     fine tuning.
+    # smpte_format - the SMPTE format. Should be 0, 24, 25, 29 or 30.
+    # smpte_offset - a Hash representing the SMPTE time offset.
+    # loops - an Array of 0 or more SamplerLoop objects containing loop point info. Loop point info
+    #         can indicate that (for example) the sampler should loop between a given sample range as long
+    #         as the sample is played.
+    # sampler_specific_data - a String of data specific to the intended target sampler, or nil if there is no sampler
+    #                         specific data.
+    #
+    # Raises InvalidFormatError if the given arguments are invalid.
     def initialize(manufacturer_id:,
                    product_id:,
                    sample_duration:,
