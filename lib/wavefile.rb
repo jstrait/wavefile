0f6459aca3fde722c7a432c0248eed5bebc5704c
Joel Strait <joel.strait@gmail.com>
2019-12-29 10:08:51 -0800

    Bumping version from 1.1.0 -> 1.1.1

--- lib/wavefile.rb
+++ lib/wavefile.rb
@@ -10,7 +10,7 @@ require 'wavefile/unvalidated_format'
 require 'wavefile/writer'
 
 module WaveFile
-  VERSION = "1.1.0"
+  VERSION = "1.1.1"
 
   WAVEFILE_FORMAT_CODE = "WAVE"    # :nodoc:
   FORMAT_CODES = {:pcm => 1, :float => 3, :extensible => 65534}.freeze    # :nodoc:
