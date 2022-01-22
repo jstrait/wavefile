c62dad5b53120a8de48e7555637146268e7f9e7d
Joel Strait <joel.strait@gmail.com>
2022-01-21 19:16:20 -0800

    More explicit labeling for unsigned integer values
    Previously unsigned integer values were labeled with an "int_"
    prefix. Now they are labeled with a "uint_" prefix, and signed
    integer values are labeled with an "int_" prefix. (Although almost
    all integer values in a *.wav file are unsigned, signed values are
    possible, e.g. in the "inst" chunk). Since elsewhere in the
    computer world signed integers are often labeled "int" and
    unsigned as "uint", the old labeling might have caused confusion.

--- tools/debug.rb
+++ tools/debug.rb
@@ -143,7 +143,7 @@ def display_chunk_header(heading, chunk_id, chunk_size)
   puts heading
   puts "=================================================================================="
   display_line "Chunk ID", "FourCC", chunk_id
-  display_line "Chunk size", "int_32", chunk_size
+  display_line "Chunk size", "uint_32", chunk_size
   display_chunk_section_separator
 end
 
@@ -156,23 +156,23 @@ end
 def read_format_chunk(chunk_id_data, chunk_size_data)
   display_chunk_header("Format Chunk", chunk_id_data, chunk_size_data)
   audio_format_code = read_bytes(UNSIGNED_INT_16)
-  display_line "Audio format",    "int_16", audio_format_code
-  display_line "Channels",        "int_16", read_bytes(UNSIGNED_INT_16)
-  display_line "Sample rate",     "int_32", read_bytes(UNSIGNED_INT_32)
-  display_line "Byte rate",       "int_32", read_bytes(UNSIGNED_INT_32)
-  display_line "Block align",     "int_16", read_bytes(UNSIGNED_INT_16)
-  display_line "Bits per sample", "int_16", read_bytes(UNSIGNED_INT_16)
+  display_line "Audio format",    "uint_16", audio_format_code
+  display_line "Channels",        "uint_16", read_bytes(UNSIGNED_INT_16)
+  display_line "Sample rate",     "uint_32", read_bytes(UNSIGNED_INT_32)
+  display_line "Byte rate",       "uint_32", read_bytes(UNSIGNED_INT_32)
+  display_line "Block align",     "uint_16", read_bytes(UNSIGNED_INT_16)
+  display_line "Bits per sample", "uint_16", read_bytes(UNSIGNED_INT_16)
 
   bytes_read_so_far = 16
 
   if audio_format_code[:actual] != 1 && chunk_size_data[:actual] > 16
     extension_size_data = read_bytes(UNSIGNED_INT_16)
-    display_line "Extension size", "int_16", extension_size_data
+    display_line "Extension size", "uint_16", extension_size_data
     bytes_read_so_far += 2
 
     if extension_size_data[:actual] > 0
       if audio_format_code[:actual] == 65534
-        display_line "Valid bits per sample", "int_16", read_bytes(UNSIGNED_INT_16)
+        display_line "Valid bits per sample", "uint_16", read_bytes(UNSIGNED_INT_16)
         display_line "Speaker mapping", "Bit field", read_bytes("B32")
         display_line "Sub format GUID", "GUID", read_bytes("H32")
 
@@ -198,7 +198,7 @@ end
 
 def read_fact_chunk(chunk_id_data, chunk_size_data)
   display_chunk_header("Fact Chunk", chunk_id_data, chunk_size_data)
-  display_line "Sample count", "int_32", read_bytes(UNSIGNED_INT_32)
+  display_line "Sample count", "uint_32", read_bytes(UNSIGNED_INT_32)
 
   if chunk_size_data[:actual] > 4
     FILE.sysread(chunk_size_data[:actual] - 4)
@@ -209,12 +209,12 @@ end
 def read_peak_chunk(chunk_id_data, chunk_size_data)
   display_chunk_header("Peak Chunk", chunk_id_data, chunk_size_data)
 
-  display_line "Version",          "int_32", read_bytes(UNSIGNED_INT_32)
-  display_line "Timestamp",        "int_32", read_bytes(UNSIGNED_INT_32)
+  display_line "Version",          "uint_32", read_bytes(UNSIGNED_INT_32)
+  display_line "Timestamp",        "uint_32", read_bytes(UNSIGNED_INT_32)
 
   ((chunk_size_data[:actual] - 8) / 8).times do |i|
     display_line "Chan. #{i + 1} Value",    "float_32", read_bytes(FLOAT_32)
-    display_line "Chan. #{i + 1} Position", "int_32", read_bytes(UNSIGNED_INT_32)
+    display_line "Chan. #{i + 1} Position", "uint_32", read_bytes(UNSIGNED_INT_32)
   end
 end
 
@@ -222,15 +222,15 @@ end
 def read_cue_chunk(chunk_id_data, chunk_size_data)
   display_chunk_header("Cue Chunk", chunk_id_data, chunk_size_data)
 
-  display_line "Cue point count", "int_32", read_bytes(UNSIGNED_INT_32)
+  display_line "Cue point count", "uint_32", read_bytes(UNSIGNED_INT_32)
 
   ((chunk_size_data[:actual] - 4) / 24).times do |i|
-    display_line "ID #{i + 1}", "int_32", read_bytes(UNSIGNED_INT_32)
-    display_line "Position #{i + 1}", "int_32", read_bytes(UNSIGNED_INT_32)
+    display_line "ID #{i + 1}", "uint_32", read_bytes(UNSIGNED_INT_32)
+    display_line "Position #{i + 1}", "uint_32", read_bytes(UNSIGNED_INT_32)
     display_line "Chunk type #{i + 1}", "FourCC", read_bytes("a4")
-    display_line "Chunk start #{i + 1}", "int_32", read_bytes(UNSIGNED_INT_32)
-    display_line "Block start #{i + 1}", "int_32", read_bytes(UNSIGNED_INT_32)
-    display_line "Sample offset #{i + 1}", "int_32", read_bytes(UNSIGNED_INT_32)
+    display_line "Chunk start #{i + 1}", "uint_32", read_bytes(UNSIGNED_INT_32)
+    display_line "Block start #{i + 1}", "uint_32", read_bytes(UNSIGNED_INT_32)
+    display_line "Sample offset #{i + 1}", "uint_32", read_bytes(UNSIGNED_INT_32)
   end
 end
 
@@ -238,31 +238,31 @@ end
 def read_sample_chunk(chunk_id_data, chunk_size_data)
   display_chunk_header("Sample Chunk", chunk_id_data, chunk_size_data)
 
-  display_line "Manufacturer",        "int_32", read_bytes(UNSIGNED_INT_32)
-  display_line "Product",             "int_32", read_bytes(UNSIGNED_INT_32)
-  display_line "Sample Period",       "int_32", read_bytes(UNSIGNED_INT_32)
-  display_line "MIDI Unity Note",     "int_32", read_bytes(UNSIGNED_INT_32)
-  display_line "MIDI Pitch Fraction", "int_32", read_bytes(UNSIGNED_INT_32)
-  display_line "SMPTEFormat",         "int_32", read_bytes(UNSIGNED_INT_32)
-  display_line "SMPTEOffset",         "int_32", read_bytes(UNSIGNED_INT_32)
+  display_line "Manufacturer",        "uint_32", read_bytes(UNSIGNED_INT_32)
+  display_line "Product",             "uint_32", read_bytes(UNSIGNED_INT_32)
+  display_line "Sample Period",       "uint_32", read_bytes(UNSIGNED_INT_32)
+  display_line "MIDI Unity Note",     "uint_32", read_bytes(UNSIGNED_INT_32)
+  display_line "MIDI Pitch Fraction", "uint_32", read_bytes(UNSIGNED_INT_32)
+  display_line "SMPTEFormat",         "uint_32", read_bytes(UNSIGNED_INT_32)
+  display_line "SMPTEOffset",         "uint_32", read_bytes(UNSIGNED_INT_32)
 
   sample_loops_bytes = read_bytes(UNSIGNED_INT_32)
   loop_count = sample_loops_bytes[:actual]
-  display_line "Sample Loops",        "int_32", sample_loops_bytes
+  display_line "Sample Loops",        "uint_32", sample_loops_bytes
 
   sampler_specific_data_size_bytes = read_bytes(UNSIGNED_INT_32)
   sampler_specific_data_size = sampler_specific_data_size_bytes[:actual]
-  display_line "Sampler Data Size",   "int_32", sampler_specific_data_size_bytes
+  display_line "Sampler Data Size",   "uint_32", sampler_specific_data_size_bytes
 
   loop_count.times do |i|
     display_chunk_section_separator
     puts "Loop ##{i + 1}:"
-    display_line "Identifier", "int_32", read_bytes(UNSIGNED_INT_32)
-    display_line "Type",       "int_32", read_bytes(UNSIGNED_INT_32)
-    display_line "Start",      "int_32", read_bytes(UNSIGNED_INT_32)
-    display_line "End",        "int_32", read_bytes(UNSIGNED_INT_32)
-    display_line "Fraction",   "int_32", read_bytes(UNSIGNED_INT_32)
-    display_line "Play Count", "int_32", read_bytes(UNSIGNED_INT_32)
+    display_line "Identifier", "uint_32", read_bytes(UNSIGNED_INT_32)
+    display_line "Type",       "uint_32", read_bytes(UNSIGNED_INT_32)
+    display_line "Start",      "uint_32", read_bytes(UNSIGNED_INT_32)
+    display_line "End",        "uint_32", read_bytes(UNSIGNED_INT_32)
+    display_line "Fraction",   "uint_32", read_bytes(UNSIGNED_INT_32)
+    display_line "Play Count", "uint_32", read_bytes(UNSIGNED_INT_32)
   end
 
   if sampler_specific_data_size > 0
@@ -280,13 +280,13 @@ end
 def read_instrument_chunk(chunk_id_data, chunk_size_data)
   display_chunk_header("Instrument Chunk", chunk_id_data, chunk_size_data)
 
-  display_line "Unshifted Note", "byte", read_bytes(UNSIGNED_INT_8)
-  display_line "Fine Tune",      "byte", read_bytes(SIGNED_INT_8)
-  display_line "Gain",           "byte", read_bytes(SIGNED_INT_8)
-  display_line "Low Note",       "byte", read_bytes(UNSIGNED_INT_8)
-  display_line "High Note",      "byte", read_bytes(UNSIGNED_INT_8)
-  display_line "Low Velocity",   "byte", read_bytes(UNSIGNED_INT_8)
-  display_line "High Velocity",  "byte", read_bytes(UNSIGNED_INT_8)
+  display_line "Unshifted Note", "uint_8", read_bytes(UNSIGNED_INT_8)
+  display_line "Fine Tune",      "int_8", read_bytes(SIGNED_INT_8)
+  display_line "Gain",           "int_8", read_bytes(SIGNED_INT_8)
+  display_line "Low Note",       "uint_8", read_bytes(UNSIGNED_INT_8)
+  display_line "High Note",      "uint_8", read_bytes(UNSIGNED_INT_8)
+  display_line "Low Velocity",   "uint_8", read_bytes(UNSIGNED_INT_8)
+  display_line "High Velocity",  "uint_8", read_bytes(UNSIGNED_INT_8)
 
   extra_data_size = chunk_size_data[:actual] - 7
   if extra_data_size > 0
@@ -310,10 +310,10 @@ def read_list_chunk(chunk_id_data, chunk_size_data)
     size_bytes = read_bytes(UNSIGNED_INT_32)
     size = size_bytes[:actual]
 
-    display_line "Size", "int_32", size_bytes
+    display_line "Size", "uint_32", size_bytes
 
     if list_type[:actual] == "adtl"
-      display_line "Cue Point ID", "int_32", read_bytes(UNSIGNED_INT_32)
+      display_line "Cue Point ID", "uint_32", read_bytes(UNSIGNED_INT_32)
       display_line "Content", "alpha_#{size - 4}", read_bytes("a#{size - 4}")
 
       bytes_remaining -= (size + 8)
