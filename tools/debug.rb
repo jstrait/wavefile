jstrait/wavefile

commit 44e0a40afe0fa40ee73a7a2db4cabe57791dc2af

Reading "RIFF" header in main chunk reading loop
This simplifies the chunk reading logic by removing a special case
for the "RIFF" chunk header.

tools/debug.rb
@@ -8,11 +8,6 @@ FLOAT_32 = "e"
 
 def main
   begin
-    # RIFF header
-    puts ""
-    display_chunk_header("Riff Chunk Header", read_bytes("a4"), read_bytes(UNSIGNED_INT_32))
-    display_line "Form type", "FourCC", read_bytes("a4")
-    puts ""
     puts ""
 
     while true
@@ -20,6 +15,8 @@ def main
       chunk_size_data = read_bytes(UNSIGNED_INT_32)
 
       case chunk_id_data[:parsed_value]
+        when "RIFF" then
+          read_riff_chunk_header(chunk_id_data, chunk_size_data)
         when "fmt " then
           read_format_chunk(chunk_id_data, chunk_size_data)
         when "fact" then
@@ -138,6 +135,12 @@ end
 
 def display_chunk_section_separator
   puts "----------------------------------+------------+----------------------------------"
+end
+
+
+def read_riff_chunk_header(chunk_id_data, chunk_size_data)
+  display_chunk_header("Riff Chunk Header", chunk_id_data, chunk_size_data)
+  display_line "Form type", "FourCC", read_bytes("a4")
 end
 
 

commit 5d12fa386c4b8c53d3209913497246220f0b8dc3

Fixing erroneous attempt to read padding byte
44e0a40afe0fa40ee73a7a2db4cabe57791dc2af introduced a bug that
caused an attempt to read a padding byte after the "form type"
field at the start of the "RIFF" chunk, if the "RIFF" chunk had an
odd size. However, a padding byte will never be in this location,
and this caused the file reader to become misaligned with the
start of the first child chunk.

Conceptually, it doesn't really seem to make sense for a "RIFF"
chunk to have an odd size, because all of its child chunks should
have an even size (when taking padding bytes into consideration).
That is, adding even numbers together should result in an another
even number. However, there isn't anything stopping a "RIFF" chunk
from reporting an odd size, and this commit makes sure that
scenario is handled properly.

tools/debug.rb
@@ -32,7 +32,7 @@ def main
       send(CHUNK_BODY_READERS[chunk_id_data[:parsed_value]], chunk_size_data[:parsed_value])
 
       # Read padding byte if necessary
-      if chunk_size_data[:parsed_value].odd?
+      if chunk_size_data[:parsed_value].odd? && chunk_id_data[:parsed_value] != "RIFF"
         display_line "Padding Byte", "byte", read_bytes(UNSIGNED_INT_8)
       end
 