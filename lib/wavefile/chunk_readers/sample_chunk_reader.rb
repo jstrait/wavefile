3500204992abefbbfcd93cdb5a87899490f1186e
Joel Strait <joel.strait@gmail.com>
2023-01-07 20:46:44 -0800

    Removing redundant assignment
    The opportunity to remove this line was accidentally overlooked in
    commit 7cc3bfccec8b045fb84b4ef27f354a08addb550a.

--- lib/wavefile/chunk_readers/sample_chunk_reader.rb
+++ lib/wavefile/chunk_readers/sample_chunk_reader.rb
@@ -46,7 +46,6 @@ module WaveFile
           loop_fields[:end_sample_frame],
           loop_fields[:fraction],
           loop_fields[:play_count] = raw_bytes.slice!(0...LOOP_BYTE_COUNT).unpack("VVVVVV")
-          loop_fields[:type] = loop_fields[:type]
           loop_fields[:fraction] /= 4_294_967_296.0
 
           fields[:loops] << SamplerLoop.new(**loop_fields)
