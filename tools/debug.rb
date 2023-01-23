156fc35fc97b015b12af3057104cc5ce27197b11
Joel Strait <joel.strait@gmail.com>
2023-01-23 11:16:29 -0800

    Debug: Better labeling for "data" chunk content
    The new labeling seems more clear and explicit.

--- tools/debug.rb
+++ tools/debug.rb
@@ -262,12 +262,12 @@ def read_data_chunk(field_reader, chunk_size)
   intro_byte_count = [10, chunk_size].min
 
   if intro_byte_count > 0
-    display_field("Data Start", field_reader.read_bytes(intro_byte_count))
+    display_field("First #{intro_byte_count} bytes", field_reader.read_bytes(intro_byte_count))
   end
 
   if intro_byte_count < chunk_size
     skipped_byte_count = field_reader.skip_bytes(chunk_size - intro_byte_count)
-    puts "(#{skipped_byte_count} bytes omitted)"
+    puts "(remaining #{skipped_byte_count} bytes omitted)"
   end
 end
 
